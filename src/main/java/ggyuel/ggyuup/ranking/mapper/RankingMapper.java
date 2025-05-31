c5645f437adcc01f55ebe0d2c8657aec05552d01
Lunabell03 <sunnism03@gmail.com>
2025-05-31 15:51:23 +0900

    [hotfix] ranking 조회 에러 해결...

--- src/main/java/ggyuel/ggyuup/ranking/mapper/RankingMapper.java
+++ src/main/java/ggyuel/ggyuup/ranking/mapper/RankingMapper.java
@@ -1,13 +1,13 @@
 package ggyuel.ggyuup.ranking.mapper;
 
-import ggyuel.ggyuup.ranking.dto.RankingRespDTO;
+import ggyuel.ggyuup.ranking.dto.SelectRankingDTO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 @Mapper
 public interface RankingMapper {
-    List<RankingRespDTO> selectEwhaRank();
+    List<SelectRankingDTO> selectEwhaRank();
 
     void insertScores(@Param("handle") String handle, @Param("total") float total, @Param("basic") float basic, @Param("rare") float rare);
     void deleteScores();
